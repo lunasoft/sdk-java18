086ae6afe8db088406ba143ff72df8aeabd64968
SW-2017-04\Soporte2 <marifer.mares@sw.com.mx>
2022-11-10 14:44:42 -0600

    UT del servicio Storage

--- src/test/java/mx/com/sw/services/storage/StorageTest.java
+++ src/test/java/mx/com/sw/services/storage/StorageTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Assertions;
 
 import mx.com.sw.exceptions.ServicesException;
 import mx.com.sw.helpers.BuildSettings;
+import mx.com.sw.services.authentication.Authentication;
+import mx.com.sw.services.authentication.responses.AuthenticationResponse;
 
 public class StorageTest {
     private final BuildSettings settings;
@@ -18,51 +20,111 @@ public class StorageTest {
         this.settings = new BuildSettings();
     }
 
-     /**
-    * Método de UT con token.
-    */
-@Test
+    /**
+     * Método de UT con token.
+     */
+    @Test
     public void testGetXmlToken_Success() {
         try {
             Storage storage = new Storage(settings.getUrlServicesSW(), settings.getTokenSW(), null, 0);
             StorageResponse res = storage.getXml(UUID.fromString("876625f9-605a-4f52-9db8-0b724c03ba38"));
             Assertions.assertNotNull(res);
             Assertions.assertTrue("success".equals(res.getStatus()));
-            Assertions.assertNotNull(res.getXml());
-            //Assertions.assertNotNull(res.);
+            Assertions.assertNotNull(res.getData());
+            Assertions.assertTrue(res.getData().getRecords().size() > 0);
         } catch (ServicesException ex) {
             Assertions.assertNotNull(ex);
         }
     }
 
     /**
-    * Método de UT con token incorrecto.
-    */
+     * Método de UT con Authentication.
+     */
+    @Test
+    public void testGetXmlAuth_Success() {
+        try {
+            Storage storage = new Storage(settings.getUrlServicesSW(), settings.getUrlSW(), settings.getUserSW(),
+                    settings.getPasswordSW(), null, 0);
+            StorageResponse res = storage.getXml(UUID.fromString("876625f9-605a-4f52-9db8-0b724c03ba38"));
+            Assertions.assertNotNull(res);
+            Assertions.assertTrue("success".equals(res.getStatus()));
+            Assertions.assertNotNull(res.getData());
+            Assertions.assertTrue(res.getData().getRecords().size() > 0);
+
+        } catch (ServicesException ex) {
+            Assertions.assertNotNull(ex);
+        }
+    }
+
+    /**
+     * Método de UT con token incorrecto.
+     */
     @Test
     public void testGetXmlToken_Error() {
         try {
             Storage storage = new Storage(settings.getUrlServicesSW(), "empty.token.sw", null, 0);
+            StorageResponse res = storage.getXml(UUID.fromString("876625f9-605a-4f52-9db8-0b724c03ba38"));
+            Assertions.assertNotNull(res);
+            Assertions.assertNotNull(res.getMessage());
+            Assertions.assertFalse("success".equals(res.getStatus()));
+        } catch (ServicesException ex) {
+            Assertions.assertNotNull(ex);
+        }
+    }
+
+    /**
+     * Método de UT con Authentication incorrecta pero Uuid correcto.
+     */
+    @Test
+    public void testGetXmlAuthIncorrect_Error() {
+        try {
+            Storage storage = new Storage(settings.getUrlServicesSW(), settings.getUrlSW(), "user",
+                    "passError", null, 0);
+            StorageResponse res = storage.getXml(UUID.fromString("876625f9-605a-4f52-9db8-0b724c03ba38"));
+            Assertions.assertNotNull(res);
+            Assertions.assertTrue("error".equals(res.getStatus()));
+            Assertions.assertNull(res.getData());
+            Assertions.assertFalse(res.getData().getRecords().size() > 0);
+        } catch (ServicesException ex) {
+            Assertions.assertNotNull(ex);
+        }
+    }
+
+    /**
+     * Método de UT con uuid incorrecto.
+     */
+    @Test
+    public void testGetXmlUuid_Error() {
+        try {
+            Storage storage = new Storage(settings.getUrlServicesSW(), settings.getTokenSW(), null, 0);
             StorageResponse res = storage.getXml(UUID.randomUUID());
             Assertions.assertNotNull(res);
-            Assertions.assertNotNull(res.getMessage());
             Assertions.assertFalse("success".equals(res.getStatus()));
         } catch (ServicesException ex) {
             Assertions.assertNotNull(ex);
         }
     }
 
+    /**
+     * Método de UT con Authentication exitoso pero uuid incorrecto
+     */
     @Test
-    public void testGetXmlEmpty_Error() {
+    public void testGetXmlAuth_Error() {
         try {
-            Storage storage = new Storage(settings.getUrlServicesSW(), "empty.token.sw", null, 0);
-            StorageResponse res = storage.getXml(UUID.fromString("876625f9-605a-4f52-9db8-0b764c03ba38"));
-            Assertions.assertNotNull(res);
-            Assertions.assertNotNull(res.getMessage());
-            Assertions.assertFalse("success".equals(res.getStatus()));
+            Authentication auth = new Authentication(settings.getUrlSW(), settings.getUserSW(),
+                    settings.getPasswordSW(), null, 0);
+            AuthenticationResponse response = auth.authenticate();
+            if (response.getStatus().equalsIgnoreCase("success")) {
+                Storage storage = new Storage(settings.getUrlServicesSW(), response.getData().getToken(), null, 0);
+                StorageResponse res = storage.getXml(UUID.fromString("876625f9-605a-4f52-9db8-0b724c03ba56"));
+                Assertions.assertNotNull(res);
+                Assertions.assertTrue("error".equals(res.getStatus()));
+                Assertions.assertNotNull(res.getData());
+                Assertions.assertFalse(res.getData().getRecords().size() > 0);
+            }
         } catch (ServicesException ex) {
             Assertions.assertNotNull(ex);
         }
     }
 
-   
 }
