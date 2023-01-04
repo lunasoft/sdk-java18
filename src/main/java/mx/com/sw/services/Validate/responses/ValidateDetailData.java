32c984f07d06b9f171308fe4a4abe1b7cf49368c
FernandoCarrilloSW <fernando.carrillo@sw.com.mx>
2023-01-03 18:19:54 -0600

    -Se construyen las clases referentes a los datos que regresa el response

---
+++ src/main/java/mx/com/sw/services/Validate/responses/ValidateDetailData.java
@@ -0,0 +1,43 @@
+package mx.com.sw.services.Validate.responses;
+
+/**
+ * ValidateDetailData Clase con la informacion detallada de cada nodo.
+ */
+public class ValidateDetailData {
+    private String message;
+    private String messageDetail;
+    private String type;
+
+    /**
+     * Retorna message de la seccion
+     * @return String
+    */
+    public String getMessage(){
+        return message;
+    }
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    /**
+     * Retorna messageDetail detallado de la seccion
+     * @return String
+    */
+    public String getMessageDetail(){
+        return messageDetail;
+    }
+    public void setMessageDetail(String messageDetail){
+        this.messageDetail = messageDetail;
+    }
+
+    /**
+     * Retorna type de la seccion
+     * @return String
+    */
+    public String getType(){
+        return type;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+}
---
+++ src/main/java/mx/com/sw/services/Validate/responses/ValidateNodeDetail.java
@@ -0,0 +1,32 @@
+package mx.com.sw.services.Validate.responses;
+import java.util.List;
+
+/**
+ * ValidateNodeDetail Clase con la informacion de los nodos de la validacion.
+ */
+public class ValidateNodeDetail {
+    private String section;
+    private List<ValidateDetailData> detail;
+
+    /**
+     * Retorna la seccion
+     * @return String
+    */
+    public String getSection(){
+        return section;
+    }
+    public void setSection(String section){
+        this.section = section;
+    }
+
+    /**
+     * Retorna datos detallados de cada seccion
+     * @return objeto tipo ValidateDetailData
+     */
+    public List<ValidateDetailData> getDetailData(){
+        return detail;
+    }
+    public void setDetailData(List<ValidateDetailData> detail){
+        this.detail = detail;
+    }
+}
